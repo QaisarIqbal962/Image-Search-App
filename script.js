cb7afca30242ba9855617b57b69d8b3b55679473
Qaisar iqbal <qaisarbangash47@gmail.com>
2025-02-10 12:18:38 +0500

    added

--- script.js
+++ script.js
@@ -5,3 +5,32 @@ const inputEl = document.getElementById("search-input")
 const searchResult = document.querySelector(".search-results")
 const showMore = document.getElementById("show-more-button")
 
+let inputData = ""
+let page = 1;
+
+async function searchImage() {
+    inputData = inputEl.value;
+    const url = `https://api.unsplash.com/search/photo?page = ${page}&query=${inputData}&client_id=${accessKey}`
+
+    const response = await fetch(url)
+    const data = await response.json()
+
+    const results = data.results
+
+    if (page === 1) {
+        searchResult.innerHTML = ""
+    }
+}
+
+
+results.map((result) => {
+    const imageWrapper = document.createElement('div')
+    imageWrapper.classList.add('search-result')
+    const image = document.createElement('img')
+    image.src = result.url.small
+    image.alt = result.alt_description
+    const imageLink = document.createElement('a')
+
+
+})
+
