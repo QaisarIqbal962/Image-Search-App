5bab42e39c68a020fc017ec113b7e83220fec3f7
Qaisar iqbal <qaisarbangash47@gmail.com>
2025-02-10 11:53:28 +0500

    javascript

---
+++ script.js
@@ -0,0 +1,7 @@
+const accessKey = "7AE_nt5EiWcOiGIVF81bpamSkLx7qn6zWNmauNF6kPQ"
+
+const formEl = document.querySelector("form")
+const inputEl = document.getElementById("search-input")
+const searchResult = document.querySelector(".search-results")
+const showMore = document.getElementById("show-more-button")
+
